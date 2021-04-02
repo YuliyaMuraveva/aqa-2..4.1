d736828d813987591ff016e2cac885ed3420bcaf
Yuliya Muraveva <dju5@ya.ru>
2021-04-02 14:30:40 +0300

    add pages

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,46 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement actionCard1 = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] button");
+    private SelenideElement actionCard2 = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] button");
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public DashboardPage() {
+        actionCard1.shouldBe(visible);
+    }
+
+    public TransferPage topUpCard1() {
+        actionCard1.click();
+        return new TransferPage();
+    }
+
+    public TransferPage topUpCard2() {
+        actionCard2.click();
+        return new TransferPage();
+    }
+
+    public int getCardBalance(String id) {
+        var text = cards.find(visible).$(By.tagName("[data-test-id=" + id + "]")).text();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+}
