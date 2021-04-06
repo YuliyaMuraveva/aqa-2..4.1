483ff7f2891474da4fddb2c41f372909732b9c3f
Yuliya <dju5@ya.ru>
2021-04-06 12:06:14 +0300

    add post actions in test

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,33 +1,29 @@
 package ru.netology.page;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
     private SelenideElement actionCard1 = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] button");
     private SelenideElement actionCard2 = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] button");
-    private ElementsCollection cards = $$(".list__item");
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
 
     public DashboardPage() {
-        actionCard1.shouldBe(visible);
+        heading.shouldBe(visible);
     }
 
-    public TransferPage topUpCard1() {
-        actionCard1.click();
-        return new TransferPage();
-    }
-
-    public TransferPage topUpCard2() {
-        actionCard2.click();
+    public TransferPage topUpCard(int card) {
+        if (card == 1) {
+            actionCard1.click();
+        }
+        else if (card == 2) {
+            actionCard2.click();
+        }
         return new TransferPage();
     }
 
--- src/test/java/ru/netology/test/TransferTest.java
+++ src/test/java/ru/netology/test/TransferTest.java
@@ -2,6 +2,7 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
 import lombok.val;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
@@ -15,6 +16,8 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TransferTest {
+    private String firstCardId = "92df3f1c-a033-48e6-8390-206f6b1f56c0";
+    private String secondCardId = "0f3f5c2a-249e-4c3d-8287-09f7a039391d";
 
     @BeforeEach
     void setup() {
@@ -26,14 +29,34 @@ public class TransferTest {
         verificationPage.validVerify(verificationCode);
     }
 
+    @AfterEach
+    void asserting() {
+        val dashboardPage = new DashboardPage();
+        int firstCardBalance = dashboardPage.getCardBalance(firstCardId);
+        int secondCardBalance = dashboardPage.getCardBalance(secondCardId);
+        if (firstCardBalance != secondCardBalance) {
+            int average = (firstCardBalance - secondCardBalance) / 2;
+            if (firstCardBalance < secondCardBalance) {
+                dashboardPage.topUpCard(1);
+                val transferPage = new TransferPage();
+                transferPage.transfer(Integer.toString(average), "5559 0000 0000 0002");
+            }
+            else {
+                dashboardPage.topUpCard(2);
+                val transferPage = new TransferPage();
+                transferPage.transfer(Integer.toString(average), "5559 0000 0000 0001");
+            }
+        }
+    }
+
     @Test
     void shouldTopUpFirstCard() {
         val dashboardPage = new DashboardPage();
-        int expected = dashboardPage.getCardBalance("92df3f1c-a033-48e6-8390-206f6b1f56c0") + 1;
-        dashboardPage.topUpCard1();
+        int expected = dashboardPage.getCardBalance(firstCardId) + 1;
+        dashboardPage.topUpCard(1);
         val transferPage = new TransferPage();
         transferPage.transfer("1", "5559 0000 0000 0002");
-        int firstCardBalance = dashboardPage.getCardBalance("92df3f1c-a033-48e6-8390-206f6b1f56c0");
+        int firstCardBalance = dashboardPage.getCardBalance(firstCardId);
         assertEquals(expected, firstCardBalance);
     }
 
@@ -41,21 +64,19 @@ public class TransferTest {
     void shouldTopUpSecondCard() {
         val dashboardPage = new DashboardPage();
         int expected = 20000;
-        String sum = Integer.toString(dashboardPage.getCardBalance("92df3f1c-a033-48e6-8390-206f6b1f56c0"));
-        dashboardPage.topUpCard2();
+        dashboardPage.topUpCard(2);
         val transferPage = new TransferPage();
-        transferPage.transfer(sum, "5559 0000 0000 0001");
-        int firstCardBalance = dashboardPage.getCardBalance("0f3f5c2a-249e-4c3d-8287-09f7a039391d");
-        assertEquals(expected, firstCardBalance);
+        transferPage.transfer("10000", "5559 0000 0000 0001");
+        int secondCardBalance = dashboardPage.getCardBalance(secondCardId);
+        assertEquals(expected, secondCardBalance);
     }
 
     @Test
     void shouldGetNotification() {
         val dashboardPage = new DashboardPage();
-        String sum = Integer.toString(dashboardPage.getCardBalance("92df3f1c-a033-48e6-8390-206f6b1f56c0") + 1);
-        dashboardPage.topUpCard2();
+        dashboardPage.topUpCard(2);
         val transferPage = new TransferPage();
-        transferPage.transfer(sum, "5559 0000 0000 0001");
+        transferPage.transfer("10001", "5559 0000 0000 0001");
         $("[data-test-id=error-notification]").$(withText("Ошибка")).shouldBe(Condition.visible);
     }
 }
